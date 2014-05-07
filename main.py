c4b253b047a12352d36f7391f5cb92a60e2a3722
JongMan Koo <jongman@gmail.com>
2014-05-08 07:22:01 +0900

    t=50 => 0.3727

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def solve(W):
 
     # 초기 온도를 정한다
     # TODO: 크기에 따라 초기 온도를 바꿔본다
-    initial_temp = 100
+    initial_temp = 50
 
     start_time = time()
     time_limit = start_time + TIME_LIMIT
